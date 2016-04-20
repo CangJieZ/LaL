dcdddff82a2349ae64aef5b06452e29e25ac895c
sean <sean@seandeMacBook-Pro.local>
2016-04-20 21:40:47 +0800

    Initial commit

---
+++ app/src/main/java/zwt/sean/lal/MainActivity.java
@@ -0,0 +1,13 @@
+package zwt.sean.lal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
