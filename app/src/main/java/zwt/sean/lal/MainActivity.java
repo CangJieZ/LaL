e4a4cb02858f1173b085bd2c0bb6694928b9b580
sean <705460418@qq.com>
2016-04-22 00:00:25 +0800

    update

--- app/src/main/java/zwt/sean/lal/MainActivity.java
+++ app/src/main/java/zwt/sean/lal/MainActivity.java
@@ -1,24 +1,89 @@
 package zwt.sean.lal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
-import Dao.UserDao;
-import greendao.User;
-import zwt.sean.lal.Base.DBMaster;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import zwt.sean.lal.main.RecordFragment;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.navigation)
+    NavigationView navigation;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.mian_content)
+    FrameLayout mianContent;
+
+    private RecordFragment recordFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_main);
-        UserDao dao = DBMaster.init().getDaoSession().getUserDao();
-        User user = new User();
-        user.setPhone("18767196567");
-        dao.insert(user);
-        User user1 = dao.queryBuilder().where(UserDao.Properties.Phone.eq("18767196567")).unique();
-        Log.e("lal",user1.getPhone()+"");
+        ButterKnife.bind(this);
+        if (null != navigation) {
+            setDrawerContent(navigation);
+        }
+        setDefaultFragment();
     }
+
+    /**
+     * 设置菜单
+     * @param navigationView
+     */
+    private void setDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.menu_record:
+                                Snackbar.make(drawerLayout, "录啊录菜单", Snackbar.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    /**
+     * 默认界面
+     */
+    private void setDefaultFragment() {
+        recordFragment = new RecordFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mian_content, recordFragment);
+        fragmentTransaction.commit();
+    }
+
 }
